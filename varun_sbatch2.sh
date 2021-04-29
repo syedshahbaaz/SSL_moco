3d13481a2b715b4a3226bd350c46d87a89750a82
syedshahbaaz <shahbaazsyed1@gmail.com>
2021-04-29 14:23:40 -0400

    Add second sbatch script

---
+++ varun_sbatch2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=08:00:00
+#SBATCH --job-name=r50cifar
+#SBATCH --partition=gpu
+#SBATCH --mem=60G
+#SBATCH --output=r50cifar.%j.out
+#SBATCH --gres=gpu:v100-sxm2:1
+module load cuda
+module load anaconda3/3.7
+source activate pytorch_env
+python pre_train.py --knn -a resnet50 --dataset cifar10 --epochs 100 --results-dir cifar10 --moco-k 32768&
+sleep 60
+python pre_train.py --knn -a resnet50 --dataset cifar10 --epochs 100 --results-dir cifar10 --moco-k 65536
